ee81aef598840653ee86ea4b49f6e42736c4b126
Akash Sengupta <alphatrionakash@gmail.com>
2020-10-26 16:01:58 +0530

    inter

--- gifthub/main/admin.py
+++ gifthub/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Admins, Users, Images, Occations, Products
 
 # Register your models here.
+
+admin.site.register(Admins)
+admin.site.register(Users)
+
+admin.site.register(Products)
+
+admin.site.register(Occations)
+admin.site.register(Images)
--- gifthub/queries.sql
+++ gifthub/queries.sql
@@ -1,9 +1,54 @@
-CREATE TABLE admin (
-  id int(11) NOT NULL,
-  name varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  pass varchar(255) NOT NULL,
+-- Create
+
+CREATE TABLE admins(
+  id INT(8) NOT NULL AUTO_INCREMENT,
+  f_name VARCHAR(255) NOT NULL,
+  l_name VARCHAR(255),
+  email VARCHAR(255) NOT NULL,
+  pass VARCHAR(255) NOT NULL,
+  token VARCHAR(255),
+  PRIMARY KEY(id)
 );
 
-ALTER TABLE admin
-ADD PRIMARY KEY(id) AUTO INCREMENT;
+CREATE TABLE users(
+  id INT(15) NOT NULL AUTO_INCREMENT,
+  f_name VARCHAR(255) NOT NULL,
+  l_name VARCHAR(255),
+  email VARCHAR(255) NOT NULL,
+  pass VARCHAR(255) NOT NULL,
+  address_1 VARCHAR(255) NOT NULL,
+  address_2 VARCHAR(255),
+  state_in VARCHAR(63) NOT NULL,
+  district VARCHAR(127) NOT NULL,
+  pincode INT(8) NOT NULL,
+  phone INT(16),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE occations(
+    id INT(15) NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE images(
+    id INT(31) NOT NULL AUTO_INCREMENT,
+    src VARCHAR(256) NOT NULL,
+    name VARCHAR(63),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE products(
+  id INT(15) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  keyword_1 VARCHAR(32) NOT NULL,
+  keyword_2 VARCHAR(32),
+  details VARCHAR(2047) NOT NULL,
+  price DECIMAL(7,2) NOT NULL,
+  occation_id INT(15) NOT NULL,
+  brand varchar(255) NOT NULL,
+  image_id INT(31) NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(occation_id) REFERENCES occations(id),
+  FOREIGN KEY(image_id) REFERENCES images(id)
+);
