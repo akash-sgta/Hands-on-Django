d6a2cc9a372eec869c04ce0a434d896a4c03431c
Akash Sengupta <alphatrionakash@gmail.com>
2020-11-04 23:33:12 +0530

    securing project

--- DJANGO/gifthub/gifthub/settings.py
+++ DJANGO/gifthub/gifthub/settings.py
@@ -19,11 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+from decouple import config
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "%9o0bkt3n=gmmkq08f-ccz+2r7g6=8lqga)g4_2%kp0h$7@d&n"
+SECRET_KEY = config('DEV_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
